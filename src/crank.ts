3cd5e4e5d29d30dd65aa5d74a1744d2f2d0d2c94
pkr <solpkr1@gmail.com>
2024-10-13 10:09:56 +0000

    auto format

--- src/crank.ts
+++ src/crank.ts
@@ -2,43 +2,43 @@ import 'dotenv/config';
 import * as os from 'os';
 import * as fs from 'fs';
 import {
-  Keypair,
-  Commitment,
-  Connection,
-  PublicKey,
-  Transaction,
-  ComputeBudgetProgram,
-  BlockhashWithExpiryBlockHeight,
-  TransactionInstruction,
+    Keypair,
+    Commitment,
+    Connection,
+    PublicKey,
+    Transaction,
+    ComputeBudgetProgram,
+    BlockhashWithExpiryBlockHeight,
+    TransactionInstruction,
 } from '@solana/web3.js';
 import BN from 'bn.js';
-import { MarketAccount, OpenBookV2Client, sleep } from '@openbook-dex/openbook-v2'
-import { AnchorProvider, Wallet } from '@coral-xyz/anchor';
+import {MarketAccount, OpenBookV2Client, sleep} from '@openbook-dex/openbook-v2'
+import {AnchorProvider, Wallet} from '@coral-xyz/anchor';
 
 function chunk<T>(array: T[], size: number): T[][] {
-  const chunkedArray: T[][] = [];
-  for (let i = 0; i < array.length; i += size) {
-    chunkedArray.push(array.slice(i, i + size));
-  }
-  return chunkedArray;
+    const chunkedArray: T[][] = [];
+    for (let i = 0; i < array.length; i += size) {
+        chunkedArray.push(array.slice(i, i + size));
+    }
+    return chunkedArray;
 }
 
 
 const {
-  RPC_URL,
-  WALLET_PATH,
-  KEYPAIR,
-  PROGRAM_ID,
-  INTERVAL,
-  CONSUME_EVENTS_LIMIT,
-  CLUSTER,
-  MARKETS, // comma separated list of market pubkeys to crank
-  PRIORITY_QUEUE_LIMIT, // queue length at which to apply the priority fee
-  PRIORITY_CU_PRICE, // extra microlamports per cu for high fee markets
-  PRIORITY_CU_LIMIT, // compute limit
-  MAX_TX_INSTRUCTIONS, // max instructions per transaction
-  CU_PRICE, // extra microlamports per cu for any transaction
-  PRIORITY_MARKETS, // input to add comma seperated list of markets that force fee bump
+    RPC_URL,
+    WALLET_PATH,
+    KEYPAIR,
+    PROGRAM_ID,
+    INTERVAL,
+    CONSUME_EVENTS_LIMIT,
+    CLUSTER,
+    MARKETS, // comma separated list of market pubkeys to crank
+    PRIORITY_QUEUE_LIMIT, // queue length at which to apply the priority fee
+    PRIORITY_CU_PRICE, // extra microlamports per cu for high fee markets
+    PRIORITY_CU_LIMIT, // compute limit
+    MAX_TX_INSTRUCTIONS, // max instructions per transaction
+    CU_PRICE, // extra microlamports per cu for any transaction
+    PRIORITY_MARKETS, // input to add comma seperated list of markets that force fee bump
 } = process.env;
 
 const cluster: 'mainnet' | 'testnet' | 'devnet' = CLUSTER as 'mainnet' | 'testnet' | 'devnet' || 'mainnet';
@@ -51,18 +51,19 @@ const priorityCuPrice = parseInt(PRIORITY_CU_PRICE || '100000');
 const CuLimit = parseInt(PRIORITY_CU_LIMIT || '50000');
 const maxTxInstructions = parseInt(MAX_TX_INSTRUCTIONS || '1');
 const programId = new PublicKey(
-  PROGRAM_ID || cluster == 'mainnet'
-    ? 'opnb2LAfJYbRMAHHvqjCwQxanZn7ReEHp1k81EohpZb'
-    : 'opnb2LAfJYbRMAHHvqjCwQxanZn7ReEHp1k81EohpZb',
+    PROGRAM_ID || cluster == 'mainnet'
+        ? 'opnb2LAfJYbRMAHHvqjCwQxanZn7ReEHp1k81EohpZb'
+        : 'opnb2LAfJYbRMAHHvqjCwQxanZn7ReEHp1k81EohpZb',
 );
-const walletFile = process.env.WALLET_PATH || os.homedir() + '/openbook-v2/ts/client/src/wallet.json';;
+const walletFile = process.env.WALLET_PATH || os.homedir() + '/openbook-v2/ts/client/src/wallet.json';
+;
 
 console.log("Loaded MARKETS:", MARKETS);
 console.log("Loaded WALLET_PATH:", WALLET_PATH);
 console.log("Loaded RPC_URL:", RPC_URL);
 
 const payer = Keypair.fromSecretKey(
-  Uint8Array.from(JSON.parse(KEYPAIR || fs.readFileSync(walletFile, 'utf-8'))),
+    Uint8Array.from(JSON.parse(KEYPAIR || fs.readFileSync(walletFile, 'utf-8'))),
 );
 const wallet = new Wallet(payer);
 const defaultRpcUrls = {
@@ -79,143 +80,143 @@ const connection = new Connection(rpcUrl, 'processed' as Commitment);
 // blockhash loop
 let recentBlockhash: BlockhashWithExpiryBlockHeight;
 try {
-  connection.getLatestBlockhash('finalized').then((blockhash) => {
-    recentBlockhash = blockhash;
-  });
+    connection.getLatestBlockhash('finalized').then((blockhash) => {
+        recentBlockhash = blockhash;
+    });
 } catch (e) {
-  console.error(`Couldn't get blockhash: ${e}`);
+    console.error(`Couldn't get blockhash: ${e}`);
 }
 setInterval(async () => {
-  try {
-    recentBlockhash = await connection.getLatestBlockhash('finalized');
-  } catch (e) {
-    console.error(`Couldn't get blockhash: ${e}`);
-  }
+    try {
+        recentBlockhash = await connection.getLatestBlockhash('finalized');
+    } catch (e) {
+        console.error(`Couldn't get blockhash: ${e}`);
+    }
 }, 1000);
 
 async function run() {
-  // list of markets to crank
-  const provider = new AnchorProvider(connection, wallet, {})
-  const client = new OpenBookV2Client(provider, programId, {});
+    // list of markets to crank
+    const provider = new AnchorProvider(connection, wallet, {})
+    const client = new OpenBookV2Client(provider, programId, {});
 
-  const marketPks = MARKETS ? MARKETS.split(',').map((m) => new PublicKey(m)) : [];
+    const marketPks = MARKETS ? MARKETS.split(',').map((m) => new PublicKey(m)) : [];
 
-  if(!marketPks.length) {
-    console.error('No valid market pubkeys provided!');
-    return;
-  }
-
-  const markets = await client.program.account.market.fetchMultiple(marketPks);
-  const eventHeapPks = markets.map((m) => m!.eventHeap);
-
-  //pass a minimum Context Slot to GMA
-  let minContextSlot = 0;
-
-  while (true) {
-    try {
-      let crankInstructionsQueue: TransactionInstruction[] = [];
-      let instructionBumpMap = new Map();
-
-      const eventHeapAccounts = await client.program.account.eventHeap.fetchMultipleAndContext(eventHeapPks);
-      const contextSlot = eventHeapAccounts[0]!.context.slot;
-      //increase the minContextSlot to avoid processing the same slot twice
-      
-      if (contextSlot < minContextSlot) {
-        console.log(`already processed slot ${contextSlot}, skipping...`)
-      }
-      minContextSlot = contextSlot + 1;
-
-      for (let i = 0; i < eventHeapAccounts.length; i++) {
-        const eventHeap = eventHeapAccounts[i]!.data;
-        const heapSize = eventHeap.header.count;
-        const market = markets[i]!;
-        const marketPk = marketPks[i];
-        if (heapSize === 0) continue;
-
-        const remainingAccounts = await client.getAccountsToConsume(market);
-        const consumeEventsIx = await client.consumeEventsIx(marketPk, market, consumeEventsLimit, remainingAccounts)
-
-        crankInstructionsQueue.push(consumeEventsIx);
-
-        //if the queue is large then add the priority fee
-        if (heapSize > priorityQueueLimit) {
-          instructionBumpMap.set(consumeEventsIx, 1);
-        }
-
-        //bump transaction fee if market address is included in PRIORITY_MARKETS env
-        if (priorityMarkets.includes(marketPk.toString())) {
-          instructionBumpMap.set(consumeEventsIx, 1);
-        }
-
-        console.log(
-          `market ${marketPk} creating consume events for ${heapSize} events (${remainingAccounts.length} accounts)`,
-        );
-      }
-      
-      //send the crank transaction if there are markets that need cranked
-      if (crankInstructionsQueue.length > 0) {
-        //chunk the instructions to ensure transactions are not too large
-        let chunkedCrankInstructions = chunk(
-          crankInstructionsQueue,
-          maxTxInstructions,
-        );
-
-        chunkedCrankInstructions.forEach((transactionInstructions) => {
-          let shouldBumpFee = false;
-          let crankTransaction = new Transaction({ ...recentBlockhash });
-
-          crankTransaction.add(
-            ComputeBudgetProgram.setComputeUnitLimit({
-              units: CuLimit * maxTxInstructions,
-            }),
-          );
-
-          transactionInstructions.forEach(function (crankInstruction) {
-            //check the instruction for flag to bump fee
-            instructionBumpMap.get(crankInstruction)
-              ? (shouldBumpFee = true)
-              : null;
-          });
-
-          if (shouldBumpFee || cuPrice) {
-            crankTransaction.add(
-              ComputeBudgetProgram.setComputeUnitPrice({
-                microLamports: shouldBumpFee ? priorityCuPrice : cuPrice,
-              }),
-            );
-          }
-
-          crankTransaction.add(...transactionInstructions);
-
-          crankTransaction.sign(payer);
-
-          //send the transaction
-          connection
-            .sendRawTransaction(crankTransaction.serialize(), {
-              skipPreflight: true,
-              maxRetries: 2,
-            })
-            .then((txId) =>
-              console.log(
-                `Cranked ${transactionInstructions.length} market(s): ${txId}`,
-              ),
-            );
-        });
-      }
-    } catch (e) {
-      if (e instanceof Error) {
-        switch (e.message) {
-          case 'Minimum context slot has not been reached':
-            //lightweight warning message for known "safe" errors
-            console.warn(e.message);
-            break;
-          default:
-            console.error(e);
-        }
-      }
+    if (!marketPks.length) {
+        console.error('No valid market pubkeys provided!');
+        return;
     }
-    await sleep(interval);
-  }
+
+    const markets = await client.program.account.market.fetchMultiple(marketPks);
+    const eventHeapPks = markets.map((m) => m!.eventHeap);
+
+    //pass a minimum Context Slot to GMA
+    let minContextSlot = 0;
+
+    while (true) {
+        try {
+            let crankInstructionsQueue: TransactionInstruction[] = [];
+            let instructionBumpMap = new Map();
+
+            const eventHeapAccounts = await client.program.account.eventHeap.fetchMultipleAndContext(eventHeapPks);
+            const contextSlot = eventHeapAccounts[0]!.context.slot;
+            //increase the minContextSlot to avoid processing the same slot twice
+
+            if (contextSlot < minContextSlot) {
+                console.log(`already processed slot ${contextSlot}, skipping...`)
+            }
+            minContextSlot = contextSlot + 1;
+
+            for (let i = 0; i < eventHeapAccounts.length; i++) {
+                const eventHeap = eventHeapAccounts[i]!.data;
+                const heapSize = eventHeap.header.count;
+                const market = markets[i]!;
+                const marketPk = marketPks[i];
+                if (heapSize === 0) continue;
+
+                const remainingAccounts = await client.getAccountsToConsume(market);
+                const consumeEventsIx = await client.consumeEventsIx(marketPk, market, consumeEventsLimit, remainingAccounts)
+
+                crankInstructionsQueue.push(consumeEventsIx);
+
+                //if the queue is large then add the priority fee
+                if (heapSize > priorityQueueLimit) {
+                    instructionBumpMap.set(consumeEventsIx, 1);
+                }
+
+                //bump transaction fee if market address is included in PRIORITY_MARKETS env
+                if (priorityMarkets.includes(marketPk.toString())) {
+                    instructionBumpMap.set(consumeEventsIx, 1);
+                }
+
+                console.log(
+                    `market ${marketPk} creating consume events for ${heapSize} events (${remainingAccounts.length} accounts)`,
+                );
+            }
+
+            //send the crank transaction if there are markets that need cranked
+            if (crankInstructionsQueue.length > 0) {
+                //chunk the instructions to ensure transactions are not too large
+                let chunkedCrankInstructions = chunk(
+                    crankInstructionsQueue,
+                    maxTxInstructions,
+                );
+
+                chunkedCrankInstructions.forEach((transactionInstructions) => {
+                    let shouldBumpFee = false;
+                    let crankTransaction = new Transaction({...recentBlockhash});
+
+                    crankTransaction.add(
+                        ComputeBudgetProgram.setComputeUnitLimit({
+                            units: CuLimit * maxTxInstructions,
+                        }),
+                    );
+
+                    transactionInstructions.forEach(function (crankInstruction) {
+                        //check the instruction for flag to bump fee
+                        instructionBumpMap.get(crankInstruction)
+                            ? (shouldBumpFee = true)
+                            : null;
+                    });
+
+                    if (shouldBumpFee || cuPrice) {
+                        crankTransaction.add(
+                            ComputeBudgetProgram.setComputeUnitPrice({
+                                microLamports: shouldBumpFee ? priorityCuPrice : cuPrice,
+                            }),
+                        );
+                    }
+
+                    crankTransaction.add(...transactionInstructions);
+
+                    crankTransaction.sign(payer);
+
+                    //send the transaction
+                    connection
+                        .sendRawTransaction(crankTransaction.serialize(), {
+                            skipPreflight: true,
+                            maxRetries: 2,
+                        })
+                        .then((txId) =>
+                            console.log(
+                                `Cranked ${transactionInstructions.length} market(s): ${txId}`,
+                            ),
+                        );
+                });
+            }
+        } catch (e) {
+            if (e instanceof Error) {
+                switch (e.message) {
+                    case 'Minimum context slot has not been reached':
+                        //lightweight warning message for known "safe" errors
+                        console.warn(e.message);
+                        break;
+                    default:
+                        console.error(e);
+                }
+            }
+        }
+        await sleep(interval);
+    }
 }
 
 run();
